fe42128f99df6c16407210512501c0ad32e6fcce
mrcruz117 <mrcruz1989@gmail.com>
2021-12-01 14:00:25 -0500

    drew dash line square

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ timmy.shape("turtle")
 timmy.color('black')
 
 
+def dash_line(length):
+    for amount in range(length):
+        timmy.forward(5)
+        timmy.penup()
+        timmy.forward(5)
+        timmy.pendown()
+
+
 def draw_square():
-    timmy.forward(200)
+    dash_line(20)
     timmy.left(90)
-    timmy.forward(200)
+    dash_line(20)
     timmy.left(90)
-    timmy.forward(200)
+    dash_line(20)
     timmy.left(90)
-    timmy.forward(200)
+    dash_line(20)
     timmy.left(90)
 
 
