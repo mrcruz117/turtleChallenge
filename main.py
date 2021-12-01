03029a9295db4fb3e2d83abb3f498d3309cbb6a9
mrcruz117 <mrcruz1989@gmail.com>
2021-12-01 13:31:00 -0500

    drew square

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from turtle import Turtle, Screen
 
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color('black')
 
 
+def draw_square():
+    timmy.forward(200)
+    timmy.left(90)
+    timmy.forward(200)
+    timmy.left(90)
+    timmy.forward(200)
+    timmy.left(90)
+    timmy.forward(200)
+    timmy.left(90)
+
+
+draw_square()
+
+screen = Screen()
+screen.exitonclick()
